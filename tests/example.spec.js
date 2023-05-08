082a2830d542c9432fb0cfed35ed220ab1462504
MariyaMitko <mariyamitko@mail.ru>
2023-05-08 15:26:55 +0300

    Add spec file

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,39 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
+const MainPage  = require('../pages/main.page');
+const IntroPage = require('../pages/intro.page');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const leftMenuItems = ["Getting Started", "Playwright Test", "Guides", "Migration", "Integrations"];
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('Base tests', () => {
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    test('Page title text', async ({ page }) => {
+        const Main = new MainPage(page);
+        await Main.openMainPage();
+        await expect(page).toHaveTitle(/Playwright/);
+    });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    test('Get started link', async ({ page }) => {
+        const Main = new MainPage(page);
+        await Main.openMainPage();
+        await Main.clickGetStarted();
+        await expect(page).toHaveURL(/.*intro/);
+    });
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+    test('Search doc modal', async ({ page }) => {
+        const Main = new MainPage(page);
+        const Intro = new IntroPage(page);
+        await Main.openMainPage();
+        await Main.clickGetStarted();
+        await Intro.clickSearch();
+        await expect(await Intro.isDocSearchPaneVisible()).toBe(true)
+    });
+
+    test('Left menu items', async ({ page }) => {
+        const Main = new MainPage(page);
+        const Intro = new IntroPage(page);
+        await Main.openMainPage();
+        await Main.clickGetStarted();
+        await expect(await Intro.getItemCount()).toStrictEqual(leftMenuItems)
+    });
+
 });
